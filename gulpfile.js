a7af98e8118b6c1dcd1106bece4cd58042870513
Felipe Travassos <contato@felipetravassos.com>
2024-06-25 15:50:46 -0300

    stable config

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp');
+
+gulp.task('default', (done) => {
+  console.log('Gulp está funcionando!'); // eslint-disable-line no-console
+  done();
+});
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/lib/styles/main.css';
+
+// Vuetify
+import { createVuetify } from 'vuetify';
+
+export default createVuetify();
